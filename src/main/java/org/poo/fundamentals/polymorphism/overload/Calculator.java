1175844c74dd58cbe64091cf875ac2895cbbec0b
henryespinoza <hespinoza@peigo.com.ec>
2024-08-03 14:02:10 -0500

    remove comments from polymorphism files - move to Readme.md

--- src/main/java/org/poo/fundamentals/polymorphism/overload/Calculator.java
+++ src/main/java/org/poo/fundamentals/polymorphism/overload/Calculator.java
@@ -1,17 +1,9 @@
 package org.poo.fundamentals.polymorphism.overload;
 
 public class Calculator {
-  // Documentation: https://www.javatpoint.com/method-overloading-in-java
-  /* Definición: El polimorfismo permite que un método pueda comportarse de diferentes maneras según el objeto
-     que lo invoque. Hay dos tipos principales de polimorfismo:
-     1. Polimorfismo de Compilación (Sobrecarga de Métodos): Permite definir múltiples métodos con el mismo nombre
-     pero con diferentes parámetros en la misma clase, proporcionando flexibilidad para usar el mismo método en
-     diferentes contextos. */
-  // Método sobrecargado para sumar dos enteros
   public int add(int a, int b) {
     return a + b;
   }
-  // Método sobrecargado para sumar dos números decimales
   public double add(double a, double b) {
     return a + b;
   }
--- src/main/java/org/poo/fundamentals/polymorphism/overwrite/Dog.java
+++ src/main/java/org/poo/fundamentals/polymorphism/overwrite/Dog.java
@@ -5,11 +5,6 @@ import org.slf4j.LoggerFactory;
 
 public class Dog extends Animal {
   private static final Logger logger = LoggerFactory.getLogger(Dog.class);
-  /* 2. Polimorfismo de Ejecución (Sobrescritura de Métodos): Permite que una subclase sobrescriba un método de su
-     superclase para proporcionar una implementación específica. Esto permite que un método se comporte
-     de manera diferente según el objeto que lo invoque. */
-  /* El polimorfismo facilita la extensibilidad y la reutilización del código, permitiendo que los objetos de
-     diferentes clases sean tratados de manera uniforme a través de una interfaz común. */
   @Override
   public void makeSound() {
     logger.info("The dog barks.");
