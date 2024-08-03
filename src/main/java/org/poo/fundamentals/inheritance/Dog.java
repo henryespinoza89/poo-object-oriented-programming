f0390a3f0cf0ec2f0fa3c5d1dbc512af7cd7a662
henryespinoza <hespinoza@peigo.com.ec>
2024-08-03 13:31:44 -0500

    remove comments from inheritance files - move to Readme.md

--- src/main/java/org/poo/fundamentals/inheritance/Dog.java
+++ src/main/java/org/poo/fundamentals/inheritance/Dog.java
@@ -5,12 +5,6 @@ import org.slf4j.LoggerFactory;
 
 public class Dog extends Animal {
   private static final Logger logger = LoggerFactory.getLogger(Dog.class);
-  /* Clase Derivada: La clase Perro hereda de Animal y agrega un nuevo atributo raza. También sobrescribe los métodos
-     hacer_sonido y describir para proporcionar una funcionalidad específica para los perros. */
-  /* Sobrescritura de Métodos: La subclase Perro redefine el método hacer_sonido para especificar que un perro ladra,
-     en lugar de hacer un sonido genérico. También redefine el método describir y llama al método describir de la
-     superclase con super().describir() para reutilizar la lógica de la superclase y agregar información específica
-     de Perro. */
   private String breed;
   public String getBreed() {
     return breed;
